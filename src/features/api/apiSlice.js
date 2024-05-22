03a5acf870d7e19b7997fcf268b51194239d2f6c
sina <sinaderik7@gmail.com>
2024-05-22 18:02:54 +0430

    feat: crud operations added to the app

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,12 +3,46 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+    tagTypes: ['todos'],
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos',
+            transformResponse: (res) =>{
+                return res.sort((a, b) => b.id - a.id)
+            },
+            providesTags: ['todos']
+        }),
+        addTodo: builder.mutation({
+            query: (todo) => ({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            }),
+            invalidatesTags: ['todos']
+        }),
+        updateTodo: builder.mutation({
+            query: (todo) => ({
+                url: `/todos/${todo.id}`,
+                method: 'PATCH',
+                body: todo
+            }),
+            invalidatesTags: ['todos']
+        }),
+        deleteTodo: builder.mutation({
+            query: ({ id }) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+                body: id
+            }),
+            invalidatesTags: ['todos']
         }),
     }),
 });
 
 // Export the generated hook
-export const { useGetTodosQuery } = apiSlice;
+export const {
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation
+} = apiSlice;
